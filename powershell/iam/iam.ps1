1229c41fac30d632d12927c93ab749ddc9b04514
xmhai <xmhai@hotmail.com>
2023-02-03 19:16:26 +0800

    powershell

---
+++ powershell/iam/iam.ps1
@@ -0,0 +1,35 @@
+$JSON = Get-Content application.json | Out-String | ConvertFrom-Json
+$lineNo = 0
+$userId = ""
+foreach($line in Get-Content request.txt) {
+	$lineNo++;
+	if($lineNo -eq 1) {
+		$userId = $line
+		Write-Host "UserId: $userId"
+		continue;
+	}
+	
+    if($line.StartsWith("Add:")){
+		$appToAdd = $line.Substring(4, $line.Length-4).Trim()
+		#Write-Host "Application to add: " $appToAdd
+		
+		# Get Application UUID
+		$appIdFound = $false
+		foreach ($application in $JSON.applications)
+		{
+			if($application.name -eq $appToAdd) {
+				$appIdFound = $true
+				$appId = $application.id
+				Write-Host "curl -x http://nexuspru.com/application/$appId/role/2349458578475874389/user/$userId"
+				break;
+			}
+		}
+		
+		if (!$appIdFound) {
+			Write-Host "WARNING: $appToAdd not found in application list json"
+		}
+    }
+}
+
+pause
+
