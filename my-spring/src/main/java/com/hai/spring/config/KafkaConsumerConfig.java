97f1078431b5b43766819e58c27b1c2a6941259c
xmhai <xmhai@hotmail.com>
2023-05-01 12:50:00 +0800

    Update kafka project

--- my-spring/src/main/java/com/hai/spring/config/KafkaConsumerConfig.java
+++ my-spring/src/main/java/com/hai/spring/config/KafkaConsumerConfig.java
@@ -5,20 +5,43 @@ import java.util.Map;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
-import org.springframework.messaging.handler.annotation.Header;
-import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.kafka.listener.ContainerProperties.AckMode;
+import org.springframework.kafka.listener.ErrorHandler;
 import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.kafka.listener.DefaultErrorHandler;
+
+import org.springframework.util.backoff.BackOff;
+import org.springframework.util.backoff.FixedBackOff;
 
 @EnableKafka
 @Configuration
 public class KafkaConsumerConfig {
+	private Long interval = 1000L; // 1s
+
+	private Long maxAttempts = 10L;
+
+	private static int messageReceived = 0;
+	
+	@Bean
+	public DefaultErrorHandler errorHandler() {
+	    BackOff fixedBackOff = new FixedBackOff(interval, maxAttempts);
+	    DefaultErrorHandler errorHandler = new DefaultErrorHandler((consumerRecord, exception) -> {
+	        // logic to execute when all the retry attemps are exhausted
+	    	System.out.println("*** All retry attemps are failed ***");
+	    }, fixedBackOff);
+	    return errorHandler;
+	}
+	
 	@Bean
 	public ConsumerFactory<String, String> consumerFactory() {
 		Map<String, Object> props = new HashMap<>();
@@ -34,16 +57,26 @@ public class KafkaConsumerConfig {
 
 		ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
 		factory.setConsumerFactory(consumerFactory());
-		factory.setConcurrency(3);
+		factory.setCommonErrorHandler(errorHandler());
+		factory.setConcurrency(1);
+		factory.getContainerProperties().setAckMode(AckMode.BATCH);
+	    factory.afterPropertiesSet();
 		return factory;
 	}
 
-	@KafkaListener(topics = "test-topic", groupId = "foo")
-	public void listen(@Payload String message, @Header(KafkaHeaders.RECEIVED_PARTITION_ID) int partition) {
+	@KafkaListener(topics = "my-topic", groupId = "foo")
+	public void listen(@Payload String message, @Header(KafkaHeaders.RECEIVED_PARTITION_ID) int partition) throws Exception {
+		messageReceived++;
 		Thread t = Thread.currentThread();
+		System.out.println("Total number of message received: " + messageReceived);
 	    System.out.println("Thread: " + t.getId() + "|" + t.getName()
 	    		+ "; Instance ID: " + System.identityHashCode(this)
 	    		+ "; From partition: " + partition
 	    		+ "; Received Message: " + message);
+	    Thread.sleep(3000);
+	    
+	    if (messageReceived>=10) {
+		    throw new Exception("Error thrown when processing the message");
+	    }
 	}
 }
--- my-spring/src/main/java/com/hai/spring/controller/KafkaController.java
+++ my-spring/src/main/java/com/hai/spring/controller/KafkaController.java
@@ -2,27 +2,37 @@ package com.hai.spring.controller;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class KafkaController {
-	private final String TOPIC = "test-topic";
+	private final String TOPIC = "my-topic";
 	
 	@Autowired
 	KafkaTemplate<String, String> kafkaTemplate;
 	
-	@GetMapping("/kafka/send")
-	public String send() throws Exception {
-		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
-		LocalDateTime now = LocalDateTime.now();
+	@GetMapping("/kafka/send/{count}")
+	public String send(@PathVariable Integer count) throws Exception {
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss:SSSSSSS");
 		
-		String message = "Current time is "+dtf.format(now);
-		kafkaTemplate.send(TOPIC, message).get(3, TimeUnit.SECONDS);
+		int numOfMessges = count==null ? 1 : count;
+		
+		for (int i=0; i<numOfMessges; i++) {
+			UUID uuid = UUID.randomUUID();
+			LocalDateTime now = LocalDateTime.now();
+			String message = dtf.format(now) + "-" + uuid.toString();
+
+			kafkaTemplate.send(TOPIC, message).get(3, TimeUnit.SECONDS);
+			System.out.println("Sent message: "+message);
+		}
+		
 		return "Message sent successfully";
 	}
 }
